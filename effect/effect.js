53d06e49f65449867be67e36b47ee074133c1e7d
NUR222X <elnurgasanovv07@gmail.com>
2026-01-28 17:30:38 +0400

    Create effect.js

---
+++ effect/effect.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const { v4: uuid } = require("uuid");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let todos = [];
+
+/* READ */
+app.get("/todos", (req, res) => res.json(todos));
+
+/* CREATE */
+app.post("/todos", (req, res) => {
+  const todo = {
+    id: uuid(),
+    title: req.body.title,
+    description: req.body.description || "",
+    completed: false
+  };
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+/* UPDATE */
+app.put("/todos/:id", (req, res) => {
+  todos = todos.map(t =>
+    t.id === req.params.id ? { ...t, ...req.body } : t
+  );
+  res.sendStatus(200);
+});
+
+/* DELETE */
+app.delete("/todos/:id", (req, res) => {
+  todos = todos.filter(t => t.id !== req.params.id);
+  res.sendStatus(204);
+});
+
+app.listen(4000, () =>
+  console.log("API → http://localhost:4000")
+);
